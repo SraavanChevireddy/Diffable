67de202e0a9d11503b0f1ed5716a98c13f9a09e4
Mounika <hnerella@amgen.com>
2022-10-12 13:50:58 -0700

    Initial Commit

---
+++ EmployeeDirectory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory
+//
+//  Created by Nerella, Hanu Naga Mounika on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
